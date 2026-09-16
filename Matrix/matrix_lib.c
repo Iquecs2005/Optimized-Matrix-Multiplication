3978fcc8485ba3df619f0e54f30eff51ed95df62
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-09-15 22:01:04 -0300

    Medium code

--- Matrix/matrix_lib.c
+++ Matrix/matrix_lib.c
@@ -2,11 +2,11 @@
 
 int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 {
-    for (int i = 0; i < r->cols; i++)
+    for (int i = 0; i < r->rows; i++)
     {
-        for (int j = 0; j < r->rows; j++)
+        for (int j = 0; j < r->cols; j++)
         {
-            int index = j * r->cols + i;
+            int index = i * r->cols + j;
             r->values[index] = scalar_value * m->values[index];
         }
     }
@@ -16,20 +16,35 @@ int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 
 int matrix_matrix_mult(matrix *m1, matrix *m2, matrix *r)
 {
-    for (int i = 0; i < r->rows; i++)
-    {
-        for (int j = 0; j < r->cols; j++)
-        {
-            int r_index = i * r->cols + j;
-            r->values[r_index] = 0;
-            for (int k = 0; k < m1->cols; k++)
-            {
-                int m1_index = i * m1->cols + k;
-                int m2_index = k * m2->cols + j;
+    float *pM1, *pM2, *pR, *plR, *pcR;
 
-                r->values[r_index] += m1->values[m1_index] * m2->values[m2_index];
+    pR = r->values;
+    for (int i = 0; i < r->cols * r->rows; i++)
+    {
+        *pR = 0;
+        pR++;
+    }
+    
+    pM1 = m1->values;
+    plR = r->values;
+    for (int i = 0; i < m1->rows; i++)
+    {
+        pM2 = m2->values;
+        
+        for (int j = 0; j < m1->cols; j++)
+        {
+            pcR = plR;
+            for (int k = 0; k < m2->cols; k++)
+            {
+                *pcR += (*pM1) * (*pM2);
+                    
+                pcR++;
+                pM2++;
             }
+            
+            pM1++;
         }
+        plR += r->cols;
     }
 
     return 0;
