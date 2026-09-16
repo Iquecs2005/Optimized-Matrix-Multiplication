80701a96e1c8e01e72e7047b82fcae5227ccb37e
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-09-15 22:12:47 -0300

    Start of advanced code

--- Matrix/matrix_lib.c
+++ Matrix/matrix_lib.c
@@ -2,13 +2,21 @@
 
 int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 {
-    for (int i = 0; i < r->rows; i++)
+    float *p_m1, *p_r;
+    int n_numbers;
+
+    p_m1 = m->values;
+    p_r = r->values;
+
+    n_numbers = r->rows * r->cols;
+
+    for (int i = 0; i < n_numbers; i++)
     {
-        for (int j = 0; j < r->cols; j++)
-        {
-            int index = i * r->cols + j;
-            r->values[index] = scalar_value * m->values[index];
-        }
+        //TODO Implement with intel intrinsics
+        *p_r = scalar_value * (*p_m1);
+
+        p_m1++;
+        p_r++;
     }
 
     return 0;
@@ -16,35 +24,39 @@ int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 
 int matrix_matrix_mult(matrix *m1, matrix *m2, matrix *r)
 {
-    float *pM1, *pM2, *pR, *plR, *pcR;
+    float *p_m1, *p_m2, *p_r, *p_l_r, *p_c_r;
+    int n_numbers;
 
-    pR = r->values;
-    for (int i = 0; i < r->cols * r->rows; i++)
+    p_r = r->values;
+    n_numbers = r->cols * r->rows;
+
+    for (int i = 0; i < n_numbers; i++)
     {
-        *pR = 0;
-        pR++;
+        *p_r = 0;
+        p_r++;
     }
     
-    pM1 = m1->values;
-    plR = r->values;
+    p_m1 = m1->values;
+    p_l_r = r->values;
     for (int i = 0; i < m1->rows; i++)
     {
-        pM2 = m2->values;
+        p_m2 = m2->values;
         
         for (int j = 0; j < m1->cols; j++)
         {
-            pcR = plR;
+            p_c_r = p_l_r;
             for (int k = 0; k < m2->cols; k++)
             {
-                *pcR += (*pM1) * (*pM2);
+                //TODO Implement with intel intrinsics
+                *p_c_r += (*p_m1) * (*p_m2);
                     
-                pcR++;
-                pM2++;
+                p_c_r++;
+                p_m2++;
             }
             
-            pM1++;
+            p_m1++;
         }
-        plR += r->cols;
+        p_l_r += r->cols;
     }
 
     return 0;
