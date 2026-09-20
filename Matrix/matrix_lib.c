c2a7cf87b187949d98fb0fb2cac07303321ff48c
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-09-20 16:12:27 -0300

    Scalar multiplication with II

--- Matrix/matrix_lib.c
+++ Matrix/matrix_lib.c
@@ -1,22 +1,30 @@
 #include "matrix_lib.h"
 
+#include <stdio.h>
+#include <immintrin.h>
+
 int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 {
     float *p_m1, *p_r;
     int n_numbers;
+    __m256 values, scalar, results;
 
     p_m1 = m->values;
     p_r = r->values;
 
     n_numbers = r->rows * r->cols;
 
-    for (int i = 0; i < n_numbers; i++)
+    scalar = _mm256_set1_ps(scalar_value);
+
+    for (int i = 0; i < n_numbers; i += 8)
     {
         //TODO Implement with intel intrinsics
-        *p_r = scalar_value * (*p_m1);
+        values = _mm256_load_ps(p_m1);
+        results = _mm256_mul_ps(values, scalar);
+        _mm256_store_ps(p_r, results);
 
-        p_m1++;
-        p_r++;
+        p_m1 += 8;
+        p_r += 8;
     }
 
     return 0;
