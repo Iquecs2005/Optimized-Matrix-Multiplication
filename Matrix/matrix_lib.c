12d810b8288a5c514e264f7b9a2a74bdf3f2d29d
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-09-20 16:50:18 -0300

    Matrix Mult with II

--- Matrix/matrix_lib.c
+++ Matrix/matrix_lib.c
@@ -18,7 +18,6 @@ int scalar_matrix_mult(float scalar_value, matrix *m, matrix *r)
 
     for (int i = 0; i < n_numbers; i += 8)
     {
-        //TODO Implement with intel intrinsics
         values = _mm256_load_ps(p_m1);
         results = _mm256_mul_ps(values, scalar);
         _mm256_store_ps(p_r, results);
@@ -35,13 +34,17 @@ int matrix_matrix_mult(matrix *m1, matrix *m2, matrix *r)
     float *p_m1, *p_m2, *p_r, *p_l_r, *p_c_r;
     int n_numbers;
 
+    __m256 values_a, values_b, results, zero;
+
     p_r = r->values;
     n_numbers = r->cols * r->rows;
 
-    for (int i = 0; i < n_numbers; i++)
+    zero = _mm256_setzero_ps();
+
+    for (int i = 0; i < n_numbers; i += 8)
     {
-        *p_r = 0;
-        p_r++;
+        _mm256_store_ps(p_r, zero);
+        p_r += 8;
     }
     
     p_m1 = m1->values;
@@ -52,14 +55,18 @@ int matrix_matrix_mult(matrix *m1, matrix *m2, matrix *r)
         
         for (int j = 0; j < m1->cols; j++)
         {
+            //Para cada elemento de A passando pelas linhas
             p_c_r = p_l_r;
-            for (int k = 0; k < m2->cols; k++)
+            values_a = _mm256_set1_ps(*p_m1);
+            for (int k = 0; k < m2->cols; k += 8)
             {
-                //TODO Implement with intel intrinsics
-                *p_c_r += (*p_m1) * (*p_m2);
+                values_b = _mm256_load_ps(p_m2);
+                results = _mm256_load_ps(p_c_r);
+                results = _mm256_fmadd_ps(values_a, values_b, results);
+                _mm256_store_ps(p_c_r, results);
                     
-                p_c_r++;
-                p_m2++;
+                p_c_r += 8;
+                p_m2 += 8;
             }
             
             p_m1++;
